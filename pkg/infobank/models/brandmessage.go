f3b3251fbd6894c645d8123b00f73394d50eaae4
jino <jino@infobank.net>
2025-06-26 16:06:45 +0900

    fix: ImageVariable 변수타입 Array 변경

--- pkg/infobank/models/brandmessage.go
+++ pkg/infobank/models/brandmessage.go
@@ -5,30 +5,30 @@ import (
 )
 
 type BrandMessage struct {
-	SenderKey              string                 `json:"senderKey" validate:"required"`
-	SendType               KakaoSendType          `json:"sendType" validate:"required"`
-	KakaoMsgType           KakaoMsgType           `json:"msgType" validate:"required"`
-	To                     string                 `json:"to" validate:"omitempty"`
-	Text                   string                 `json:"text,omitempty"`
-	ImgUrl                 string                 `json:"imgUrl,omitempty"`
-	Targeting              string                 `json:"targeting,omitempty"`
-	TemplateCode           string                 `json:"templateCode,omitempty"`
-	Button                 []KakaoButton          `json:"button,omitempty"`
-	Fallback               *Fallback              `json:"fallback,omitempty"`
-	GroupTagKey            string                 `json:"groupTagKey,omitempty"`
-	Adult                  string                 `json:"adult,omitempty"`
-	PushAlarm              string                 `json:"pushAlarm,omitempty"`
-	MessageVariable        map[string]interface{} `json:"messageVariable,omitempty"`
-	ButtonVariable         map[string]interface{} `json:"buttonVariable,omitempty"`
-	CouponVariable         map[string]interface{} `json:"couponVariable,omitempty"`
-	ImageVariable          map[string]interface{} `json:"imageVariable,omitempty"`
-	VideoVariable          map[string]interface{} `json:"videoVariable,omitempty"`
-	CommerceVariable       map[string]interface{} `json:"commerceVariable,omitempty"`
-	CarouselVariable       map[string]interface{} `json:"carouselVariable,omitempty"`
-	OriginCID              string                 `json:"originCID,omitempty"`
-	UnsubscribePhoneNumber string                 `json:"unsubscribePhoneNumber,omitempty"`
-	UnsubscribeAuthNumber  string                 `json:"unsubscribeAuthNumber,omitempty"`
-	Ref                    string                 `json:"ref,omitempty"`
+	SenderKey              string                   `json:"senderKey" validate:"required"`
+	SendType               KakaoSendType            `json:"sendType" validate:"required"`
+	KakaoMsgType           KakaoMsgType             `json:"msgType" validate:"required"`
+	To                     string                   `json:"to" validate:"omitempty"`
+	Text                   string                   `json:"text,omitempty"`
+	ImgUrl                 string                   `json:"imgUrl,omitempty"`
+	Targeting              string                   `json:"targeting,omitempty"`
+	TemplateCode           string                   `json:"templateCode,omitempty"`
+	Button                 []KakaoButton            `json:"button,omitempty"`
+	Fallback               *Fallback                `json:"fallback,omitempty"`
+	GroupTagKey            string                   `json:"groupTagKey,omitempty"`
+	Adult                  string                   `json:"adult,omitempty"`
+	PushAlarm              string                   `json:"pushAlarm,omitempty"`
+	MessageVariable        map[string]interface{}   `json:"messageVariable,omitempty"`
+	ButtonVariable         map[string]interface{}   `json:"buttonVariable,omitempty"`
+	CouponVariable         map[string]interface{}   `json:"couponVariable,omitempty"`
+	ImageVariable          []map[string]interface{} `json:"imageVariable,omitempty"`
+	VideoVariable          map[string]interface{}   `json:"videoVariable,omitempty"`
+	CommerceVariable       map[string]interface{}   `json:"commerceVariable,omitempty"`
+	CarouselVariable       map[string]interface{}   `json:"carouselVariable,omitempty"`
+	OriginCID              string                   `json:"originCID,omitempty"`
+	UnsubscribePhoneNumber string                   `json:"unsubscribePhoneNumber,omitempty"`
+	UnsubscribeAuthNumber  string                   `json:"unsubscribeAuthNumber,omitempty"`
+	Ref                    string                   `json:"ref,omitempty"`
 }
 
 func (m *BrandMessage) Validate() error {
